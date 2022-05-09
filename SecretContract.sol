1fc463b9430c065301b02b5ab6d09226082aa5ca
Kim-Changgyu <kimc980106@naver.com>
2022-05-10 03:02:35 +0900

    스마트 컨트랙트 수정
    1. 저장 및 리턴 방식 변경
    2. 날짜 추가

--- SecretContract.sol
+++ SecretContract.sol
@@ -4,27 +4,41 @@ pragma solidity >= 0.8.13;
 
 contract SecretContract {
     struct Instance {
-        address author;                             // 갑
-        address target;                             // 을
-        string content;                             // 계약 내용
+        address A;                                    // A
+        address B;                                    // B
+        string content;                               // 계약 내용
+        string date;                                  // 작성일자자
     }
 
-    address public owner;                           // 스마트 컨트랙트 소유주
-
-    mapping (address => Instance[]) contracts;      // 어드레스별 계약서 관리
+    address public owner;                             // 스마트 컨트랙트 소유주
+    mapping (uint => Instance) contracts;             // 어드레스별 계약서 관리
+    uint counter;
 
     constructor () {
         owner = msg.sender;
+        counter = 0;
     }
 
     // 계약서 조회
-    function getContract() view public returns (Instance[] memory) {
-        return contracts[msg.sender];
+    function getContract() view public returns (address[] memory, address[] memory, string[] memory, string[] memory) {
+        address[] memory _A = new address[](counter);
+        address[] memory _B = new address[](counter);
+        string[] memory _content = new string[](counter);
+        string[] memory _date = new string[](counter);
+
+        for(uint i = 0; i < counter; i++) {
+            _A[i] = contracts[i].A;
+            _B[i] = contracts[i].B;
+            _content[i] = contracts[i].content;
+            _date[i] = contracts[i].date;
+        }
+
+        return (_A, _B, _content, _date);
     }
 
     // 계약서 등록
-    function addContract(address _target, string memory _content) public {
-        contracts[msg.sender].push(Instance(msg.sender, _target, _content));
+    function addContract(address _A, address _B, string memory _content, string memory _date) public {
+        contracts[counter++] = Instance(_A, _B, _content, _date);
     }
 
     // 계약 잔고 조회
