debea4a4cdca18e1f2669bb201615454b0763cbb
Kim-Changgyu <kimc980106@naver.com>
2022-05-10 14:50:06 +0900

    스마트 컨트랙트 및 소스코드 수정

--- SecretContract.sol
+++ SecretContract.sol
@@ -12,11 +12,13 @@ contract SecretContract {
 
     address public owner;                             // 스마트 컨트랙트 소유주
     mapping (uint => Instance) contracts;             // 어드레스별 계약서 관리
-    uint counter;
+    uint counter;                                     // 전체 계약서 수수
+    uint commision;                                   // 수수료
 
     constructor () {
         owner = msg.sender;
         counter = 0;
+        commision = 10000000000000000;                // 기본 설정 : 0.01 Ether
     }
 
     // 계약서 조회
@@ -37,12 +39,36 @@ contract SecretContract {
     }
 
     // 계약서 등록
-    function addContract(address _A, address _B, string memory _content, string memory _date) public {
+    function addContract(address _A, address _B, string memory _content, string memory _date) public payable {
+        if (msg.value != commision) {
+            revert();
+        }
+        
         contracts[counter++] = Instance(_A, _B, _content, _date);
+    }
+
+
+    // 등록 수수료 조회
+    function getCommision() view public returns (uint) {
+        return commision;
+    }
+
+    // 소유주 조회
+    function getOwner() view public returns (address) {
+        return owner;
     }
 
     // 계약 잔고 조회
     function getBalance() view public returns (uint) {
         return address(this).balance;
+    }
+
+    // 전액 잔고 회수
+    function withdraw() public payable {
+        require(msg.sender == owner);
+
+        if (!payable(owner).send(address(this).balance)) {
+            revert();
+        }
     }
 }
